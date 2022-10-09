60f6d6133d861062d473975fca7f27ddcb87bdbc
xoxoedg <rugyasin@gmail.com>
2022-10-09 15:10:08 +0200

    Modal Close Logic

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -9,7 +9,7 @@ function Header(props) {
     return <Fragment>
         <header className={classes.header}>
             <h1>ReactMeals</h1>
-            <HeaderCartButton/>
+            <HeaderCartButton onClick={props.onShowCart}/>
         </header>
         <div className={"main-image"}>
             <img src={mealsImage}/>
--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -2,8 +2,8 @@ import classes from "./Modal.module.css"
 import {Fragment} from "react";
 import ReactDOM from "react-dom";
 
-function Backdrop() {
-    return <div className={classes.backdrop}/>
+function Backdrop(props) {
+    return <div className={classes.backdrop} onClick={props.onClose}/>
 }
 
 function ModalOverlay(props) {
@@ -20,7 +20,7 @@ function Modal(props) {
 
     return (
         <Fragment>
-            {ReactDOM.createPortal(<Backdrop/>, portalElement)}
+            {ReactDOM.createPortal(<Backdrop onClose={props.onClose}/>, portalElement)}
             {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>, portalElement)}
         </Fragment>
     )
